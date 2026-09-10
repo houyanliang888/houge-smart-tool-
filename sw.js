ccbce43531bba3c84bc7ac6977e795cc45e00d97
侯哥 <houge@local>
2026-09-10 16:25:16 +0800

    fix: 图标改名icon-v2并提升SW缓存版本至dwapp-v2，强制刷新旧缓存图标

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dwapp-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='dwapp-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-v2-192.png','./icon-v2-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
